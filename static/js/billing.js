fc818ced111bd6c4af07184d2b7f0000987de34b
mmoolloo <luis@pollstr.io>
2018-04-24 13:49:51 -0500

    Added billing calculations and stats
    Added numbers to billing, as well as a total line in the graph

--- static/js/billing.js
+++ static/js/billing.js
@@ -38,7 +38,10 @@ var febLabels = [],
   marDataRD = [],
   aprDataS = [],
   aprDataR = [],
-  aprDataRD = [];
+  aprDataRD = [],
+  febTotal = [],
+  marTotal = [],
+  aprTotal = [];
 
 for (var i = 1; i <= 28; i++) {
     febLabels.push(i + ' Feb');
@@ -55,11 +58,11 @@ for (var i = 1; i <= 30; i++) {
 
 
 for (var i = 1; i <= 28; i++) {
-    febDataS.push(Math.floor(Math.random() * 1500));
+    febDataS.push(Math.floor(Math.random() * 1000));
 }
 
 for (var i = 1; i <= 28; i++) {
-    febDataRD.push(Math.floor(Math.random() * 455));
+    febDataRD.push(Math.floor(Math.random() * 220));
 }
 
 febDataS.sort(function(a, b){return a - b});
@@ -69,14 +72,18 @@ for(var i = 0; i <= febDataRD.length - 1; i++) {
   febDataR.push(febDataS[i] - febDataRD[i]);
 }
 
+for(var i = 0; i <= febDataS.length - 1; i++) {
+  febTotal.push(febDataS[i] + febDataR[i]);
+}
+
 
 
 for (var i = 1; i <= 31; i++) {
-    marDataS.push(Math.floor(Math.random() * 1500));
+    marDataS.push(Math.floor(Math.random() * 1000));
 }
 
 for (var i = 1; i <= 31; i++) {
-    marDataRD.push(Math.floor(Math.random() * 455));
+    marDataRD.push(Math.floor(Math.random() * 220));
 }
 
 marDataS.sort(function(a, b){return a - b});
@@ -86,13 +93,17 @@ for(var i = 0; i <= marDataRD.length - 1; i++) {
   marDataR.push(marDataS[i] - marDataRD[i]);
 }
 
+for(var i = 0; i <= marDataS.length - 1; i++) {
+  marTotal.push(marDataS[i] + marDataR[i]);
+}
+
 
 for (var i = 1; i <= 30; i++) {
-    aprDataS.push(Math.floor(Math.random() * 1500));
+    aprDataS.push(Math.floor(Math.random() * 1000));
 }
 
 for (var i = 1; i <= 30; i++) {
-    aprDataRD.push(Math.floor(Math.random() * 455));
+    aprDataRD.push(Math.floor(Math.random() * 220));
 }
 
 aprDataS.sort(function(a, b){return a - b});
@@ -102,23 +113,85 @@ for(var i = 0; i <= aprDataRD.length - 1; i++) {
   aprDataR.push(aprDataS[i] - aprDataRD[i]);
 }
 
+for(var i = 0; i <= aprDataS.length - 1; i++) {
+  aprTotal.push(aprDataS[i] + aprDataR[i]);
+}
+
+var costPerLine = 10
+
+function bStats(month){
+  var statsCont = document.createElement('div');
+  statsCont.classList.add('row','container');
+  var bigCont = document.getElementById('billing-data');
+  bigCont.appendChild(statsCont);
+
+  var interactions = document.createElement('p');
+  interactions.classList.add('grey-text','text-darken-2','right-align','flow-text')
+  var intNumCont = document.createElement('span');
+  intNumCont.classList.add('b-orange-text');
+  statsCont.appendChild(interactions);
+  var intTxt = document.createTextNode('Número de interacciones: ')
+  interactions.appendChild(intTxt);
+  interactions.appendChild(intNumCont);
+  if(month == 'Febrero'){
+    var intNum = document.createTextNode(febTotal[febTotal.length - 1]);
+    var tCosNum = document.createTextNode('$' + Math.round(febTotal[febTotal.length - 1] * costPerLine * 116) / 100);
+  }
+  else if (month == 'Marzo') {
+    var intNum = document.createTextNode(marTotal[marTotal.length - 1]);
+    var tCosNum = document.createTextNode('$' + Math.round(marTotal[marTotal.length - 1] * costPerLine * 116) / 100);
+  }
+  else if (month == 'Abril') {
+    var intNum = document.createTextNode(aprTotal[aprTotal.length - 1]);
+    var tCosNum = document.createTextNode('$' + Math.round(aprTotal[aprTotal.length - 1] * costPerLine * 116) / 100);
+  }
+  intNumCont.appendChild(intNum);
+
+  var cost = document.createElement('p');
+  cost.classList.add('grey-text','text-darken-2','right-align','flow-text')
+  var cosNumCont = document.createElement('span');
+  cosNumCont.classList.add('b-orange-text');
+  statsCont.appendChild(cost);
+  var cosTxt = document.createTextNode('Costo por interacción: ')
+  cost.appendChild(cosTxt);
+  cost.appendChild(cosNumCont);
+  var cosNum = document.createTextNode('$' + costPerLine + ' + I.V.A.');
+  cosNumCont.appendChild(cosNum);
+
+  var tCost = document.createElement('p');
+  tCost.classList.add('grey-text','text-darken-2','right-align','flow-text')
+  var tCosNumCont = document.createElement('span');
+  tCosNumCont.classList.add('b-orange-text');
+  statsCont.appendChild(tCost);
+  var tCosTxt = document.createTextNode('Costo total del período: ')
+  tCost.appendChild(tCosTxt);
+  tCost.appendChild(tCosNumCont);
+  tCosNumCont.appendChild(tCosNum);
+}
 
 function billingFebrero(){
   var ctx = document.getElementById("consumo").getContext('2d');
   var lineData = {
       labels: febLabels,
       datasets: [{
-          label: 'Respondidas',
+          label: 'Msjs. Enviados',
+          data: febDataS,
+          backgroundColor: pollstrChartColors(0)[1],
+          borderColor: pollstrChartColors(1)[1],
+          borderWidth: 1
+      },
+      {
+          label: 'Respuestas',
           data: febDataR,
-          backgroundColor: pollstrChartColors(overlappingOpacity)[0],
+          backgroundColor: pollstrChartColors(0)[0],
           borderColor: pollstrChartColors(1)[0],
           borderWidth: 1
       },
       {
-          label: 'Enviadas',
-          data: febDataS,
-          backgroundColor: pollstrChartColors(overlappingOpacity)[1],
-          borderColor: pollstrChartColors(1)[1],
+          label: 'Total',
+          data: febTotal,
+          backgroundColor: pollstrChartColors(overlappingOpacity)[4],
+          borderColor: pollstrChartColors(1)[4],
           borderWidth: 1
       }]
   };
@@ -127,6 +200,7 @@ function billingFebrero(){
       data: lineData,
       options: lineOptions
   });
+  bStats('Febrero');
 }
 
 
@@ -136,17 +210,24 @@ function billingMarzo(){
   var lineData = {
       labels: marLabels,
       datasets: [{
-          label: 'Respondidas',
+          label: 'Msjs. Enviados',
+          data: marDataS,
+          backgroundColor: pollstrChartColors(0)[1],
+          borderColor: pollstrChartColors(1)[1],
+          borderWidth: 1
+      },
+      {
+          label: 'Respuestas',
           data: marDataR,
-          backgroundColor: pollstrChartColors(overlappingOpacity)[0],
+          backgroundColor: pollstrChartColors(0)[0],
           borderColor: pollstrChartColors(1)[0],
           borderWidth: 1
       },
       {
-          label: 'Enviadas',
-          data: marDataS,
-          backgroundColor: pollstrChartColors(overlappingOpacity)[1],
-          borderColor: pollstrChartColors(1)[1],
+          label: 'Total',
+          data: marTotal,
+          backgroundColor: pollstrChartColors(overlappingOpacity)[4],
+          borderColor: pollstrChartColors(1)[4],
           borderWidth: 1
       }]
   };
@@ -155,6 +236,7 @@ function billingMarzo(){
       data: lineData,
       options: lineOptions
   });
+  bStats('Marzo');
 }
 
 function billingAbril(){
@@ -162,17 +244,24 @@ function billingAbril(){
   var lineData = {
       labels: aprLabels,
       datasets: [{
-          label: 'Respondidas',
+          label: 'Msjs. Enviados',
+          data: aprDataS,
+          backgroundColor: pollstrChartColors(0)[1],
+          borderColor: pollstrChartColors(1)[1],
+          borderWidth: 1
+      },
+      {
+          label: 'Respuestas',
           data: aprDataR,
-          backgroundColor: pollstrChartColors(overlappingOpacity)[0],
+          backgroundColor: pollstrChartColors(0)[0],
           borderColor: pollstrChartColors(1)[0],
           borderWidth: 1
       },
       {
-          label: 'Enviadas',
-          data: aprDataS,
-          backgroundColor: pollstrChartColors(overlappingOpacity)[1],
-          borderColor: pollstrChartColors(1)[1],
+          label: 'Total',
+          data: aprTotal,
+          backgroundColor: pollstrChartColors(overlappingOpacity)[4],
+          borderColor: pollstrChartColors(1)[4],
           borderWidth: 1
       }]
   };
@@ -181,4 +270,5 @@ function billingAbril(){
       data: lineData,
       options: lineOptions
   });
+  bStats('Abril');
 }
