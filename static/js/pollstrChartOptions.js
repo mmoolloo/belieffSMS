59860a46f0af4ecd3f2aec21684f39d26c1f46be
mmoolloo <luis@pollstr.io>
2018-04-24 14:35:42 -0500

    Changed colors
    From ugly red to purple

--- static/js/pollstrChartOptions.js
+++ static/js/pollstrChartOptions.js
@@ -7,10 +7,10 @@ function pollstrChartColors(opacity){
       'rgba(240, 88, 72,' + opacity + ')',
       'rgba(16, 152, 213,' + opacity + ')',
       'rgba(236, 26, 82,' + opacity + ')',
-      'rgba(193, 39, 45,' + opacity + ')',
-      'rgba(98, 31, 96,' + opacity + ')',
+      'rgba(95, 83, 163,' + opacity + ')',
       'rgba(8, 69, 110,' + opacity + ')',
       'rgba(240, 102, 141,' + opacity + ')',
+      'rgba(98, 31, 96,' + opacity + ')',
       'rgba(251, 174, 25,' + opacity + ')',
       'rgba(87, 187, 193,' + opacity + ')'
   ]
